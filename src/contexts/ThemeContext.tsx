9ee234bb349157ddc4921a9bd96905719fc16084
Azka Norouzi <a.azkanorouzi@gmail.com>
2024-05-15 18:35:23 +0330

    Add useLocalStorage, and implement theme selection preservation throughout reloads

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,27 +1,28 @@
-import { useContext, useState } from 'react'
-import { createContext } from 'react'
+import useLocalStorage from "@/hooks/useLocalStorage";
+import { useContext } from "react";
+import { createContext } from "react";
 
 const ThemeContext = createContext(
   {} as {
-    theme: string
-    setTheme: React.Dispatch<React.SetStateAction<string>>
-  }
-)
+    theme: string;
+    setTheme: React.Dispatch<React.SetStateAction<string>>;
+  },
+);
 
 export default function ThemeContextProvider({ children }) {
-  const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useLocalStorage("theme", "dark");
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
     </ThemeContext.Provider>
-  )
+  );
 }
 
 export function useTheme() {
-  const context = useContext(ThemeContext)
+  const context = useContext(ThemeContext);
   if (!context) {
-    throw new Error('The theme context is being use outside of its context')
+    throw new Error("The theme context is being use outside of its context");
   }
-  return context
+  return context;
 }
